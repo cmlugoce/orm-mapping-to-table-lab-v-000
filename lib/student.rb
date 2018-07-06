1d4ab13a43c7283d2e2ae4418a05ff69fd3e7475
Cristina Lugo <cmlugoce@syr.edu>
2018-07-06 14:26:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,5 +9,14 @@ class Student
     @grade = grade
     @id = some_id
   end 
-  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
 end
