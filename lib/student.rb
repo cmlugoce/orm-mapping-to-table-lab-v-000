c42b521d56adad40b2b258fe377de56b7a8ce73c
Cristina Lugo <cmlugoce@syr.edu>
2018-07-06 14:28:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,12 +11,14 @@ class Student
   end 
   def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        album TEXT
+        grade TEXT
         )
         SQL
     DB[:conn].execute(sql) 
   end
+  def self.drop_table
+    
 end
